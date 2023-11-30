723805464303abee5cf52f573ffb92f917aa3544
ZhaofangHan <132046866+ZhaofangHan@users.noreply.github.com>
2023-11-30 16:26:46 +0800

    Create muscle_Gblocks.sh

---
+++ phylogeny_tree/muscle_Gblocks.sh
@@ -0,0 +1,11 @@
+for i in  `ls ../SingleCopyOrthologGroups/*.fasta | sed 's/..\/SingleCopyOrthologGroups\///g'`
+do
+        echo $i 
+        /public/tools/alignment/muscle-3.8.1551/muscle -in ../SingleCopyOrthologGroups/${i} -out ${i}.1
+        ~/bioSoft/Gblocks_0.91b/Gblocks ${i}.1 -b4=5 -b5=h -e=.2
+
+        seqkit sort ${i}.1.2 > ${i}.1.2.3
+        seqkit seq ${i}.1.2.3 -w 0 > ${i}.1.2.3.4
+done
+
+paste -d " " *.4 > all.fa
